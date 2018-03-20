6f908b94987d99805ac1dbe421a589274b1d8a2a
thomasroest <thomasroest@outlook.com>
2018-03-20 13:48:13 +0100

    add spinner

--- src/App.js
+++ src/App.js
@@ -79,12 +79,37 @@ function isSearched(searchTerm) {
   }
 }
 
+function DataContainer(props) {
+  debugger;
+  if(props.isLoading == 'progress') {
+    return (
+      <div class="spinner">
+      <div class="rect1"></div>
+      <div class="rect2"></div>
+      <div class="rect3"></div>
+      <div class="rect4"></div>
+      <div class="rect5"></div>
+    </div>
+  )
+  } else {
+    
+    return (
+      props.items.map(item => (
+        <ListItem key={item.objectID} details={item} />
+      ))
+    )
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { searchTerm: "", hn_posts: [], test_items: test_items }
+    this.state = { 
+      searchTerm: "", 
+      hn_posts: [], 
+      test_items: test_items,
+      isLoading: 'finished' }
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
     this.loadData = this.loadData.bind(this);
@@ -103,6 +128,7 @@ class App extends Component {
   
   
   componentDidMount() {
+    // this.setState({ isLoading: 'progress'})
     const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
     axios.get(url).then(res => {
       const posts = res.data.hits;
@@ -113,9 +139,10 @@ class App extends Component {
   loadData(e) {
     const QUERY = e.target.textContent.toLowerCase();
     const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${QUERY}`;
+    this.setState({ isLoading: 'progress'})
     axios.get(url).then(res => {
       const posts = res.data.hits;
-      this.setState({ hn_posts: posts });
+      this.setState({ hn_posts: posts, isLoading: 'finished' });
     });
   }
 
@@ -135,9 +162,10 @@ class App extends Component {
             <span className="list-item__meta">points</span>
             <span className="list-item__meta">num_comments</span>
           </li>
-          {this.state.hn_posts.map(item => (
-            <ListItem key={item.objectID} details={item} />
-          ))}
+          
+          <DataContainer isLoading={this.state.isLoading} items={this.state.hn_posts}/>
+          
+          
           {/* {this.state.hn_posts.filter(isSearched(this.state.searchTerm)).map(item => (
             <ListItem key={item.objectID} details={item} />
           ))} */}
